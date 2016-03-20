b9cd898db2dbff0ecc6f5f61f01d759090aaeb63
Humza <HumzaCodeMail@gmail.com>
2016-03-20 03:40:12 -0500

    Clear white spaces in between squares

--- script.js
+++ script.js
@@ -1,14 +1,42 @@
 $(document).ready(function() {
 	for(var j=0;j<16;j++)
 	{
-		for(var i=0;i<16;i++)
+		var lastRow;
+		if(j===15)
 		{
-			$('#wrapper').append('<div class="square"></div>');
+			lastRow = true;
+		}
+		else
+		{
+			lastRow = false;
+		}
+		if(!lastRow)
+		{
+			for(var i=0;i<15;i++)
+			{
+				$('#center').append('<div class="square"></div>');
+			}
+			$('#center').append('<div class="square lastCol"></div>');
+		}
+		else
+		{
+			for(var i=0;i<15;i++)
+			{
+				$('#center').append('<div class="square lastRow"></div>');
+			}
+			$('#center').append('<div class="square lastRow lastCol"></div>');
 		}
 	}
+
+	var sqSide = 650/16
+
+	$('#center').width('747px');
 	$('.square').css('border','solid');
+	$('.square').css('margin-top','-4px');
 	$('.square').css('display','inline-block');
-	$('.square').width('75px');
-	$('.square').height('75px');
+	$('.square').width(sqSide);
+	$('.square').height(sqSide);
+	$('.lastCol').css('margin-left','-1px');
+	$('.lastRow').css('margin-top','-6px');
 	
 });
